10b79c4d4486fa7ad59058fade79a3db41c340de
Ritika Chawla <148684410+Ritikachawlaa@users.noreply.github.com>
2026-03-03 21:58:11 +0530

    updates

--- ml/modules/object_detection/detector.py
+++ ml/modules/object_detection/detector.py
@@ -5,8 +5,8 @@ Detects a wide variety of common objects (COCO classes).
 from utils.base_detector import BaseDetector
 
 class ObjectDetector(BaseDetector):
-    def __init__(self, conf=0.65):
-        # Using yolov8n.pt (Nano) for speed and higher conf for strictness
+    def __init__(self, conf=0.35):
+        # Using yolov8n.pt (Nano) for speed; conf=0.35 to detect everyday objects
         super().__init__(model_path="yolov8n.pt", conf=conf)
 
     def detect_all(self, frame):
